f6d17e82fb4ac1932018b7d140f76ff9b5fea63a
Gabriel Matias <ggezcom991gh@gmail.com>
2024-12-16 19:51:53 -0300

    Teste de responsividade 2.

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ function generatePDF() {
     else if(document.getElementById("CheckBoxCaixaDeMadeira").checked){
         text += document.getElementById("CheckBoxCaixaDeMadeira").value + "\n"
     }
-    else if(document.getElementById("CheckBoxSemCaixa").checked){
-        text += document.getElementById("CheckBoxSemCaixa").value + "\n"
+    else if(document.getElementById("CheckBoxPallet").checked){
+        text += document.getElementById("CheckBoxPallet").value + "\n"
     }
     else{
         alert("Marque o material da caixa.")
