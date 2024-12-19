b5a3a93aaf8b3b6787e95cb114c59a5c70a019cc
Gabriel Matias <ggezcom991gh@gmail.com>
2024-12-18 22:02:24 -0300

    Adicionando mais checkBoxs.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var downloadButton = document.getElementById("MyButton")
+var DivDeArmazenarImagens = document.getElementById("ArmazenarImagens")
+var labelDeArmazenarImagens = document.getElementById("QuestaoArquivo")
 
 function EffectButton(){
     downloadButton.style.backgroundColor = "black"
@@ -9,21 +11,52 @@ function EffectButton(){
     }, 200);
 }
 
+function ExibirImagensNaTela(event) {
+    var input = event.target
+
+    Array.from(input.files).forEach(file => {
+        var reader = new FileReader()
+        reader.onload = function() {
+            var dataURL = reader.result
+            var img = document.createElement("img")
+            img.className = "ImagensAnexadas"
+            img.src = dataURL
+            labelDeArmazenarImagens.appendChild(img)
+            console.log(dataURL)
+        }
+        reader.readAsDataURL(file)
+    })
+}
+
+function RemoverImagens() {
+    DivDeArmazenarImagens.removeChild(DivDeArmazenarImagens, lastChild)
+}
+
 function generatePDF() {
 
     var text = ""
 
-    //Número de série/agrupador
-    if(document.getElementById("SerieCaixaDeTexto") != ""){
-        text += document.getElementById("SerieCaixaDeTexto").value + "\n"
+    //Número de NF
+    if(document.getElementById("NFCaixaDeTexto").value != ""){
+        text += document.getElementById("NFCaixaDeTexto").value + "\n"
     }else{
-        alert("Preencha o número de série!")
+        alert("Preencha o número de NF!")
         return
     }
-    if(document.getElementById("AgrupadorCaixaDeTexto") != ""){
-        text += document.getElementById("AgrupadorCaixaDeTexto").value + "\n"
-    }else{
-        alert("Preencha o número de agrupador!")
+
+    //Estado e Cidade
+    if(document.getElementById("EstadoCampoDeTexto").value != ""){
+        text += document.getElementById("EstadoCampoDeTexto").value + "\n"
+    }
+    else{
+        alert("Preencha o campo de Estado!")
+        return
+    }
+    if(document.getElementById("CidadeCampoDeTexto").value != ""){
+        text += document.getElementById("CidadeCampoDeTexto").value + "\n"
+    }
+    else{
+        alert("Preencha o campo de Cidade!")
         return
     }
 
@@ -42,18 +75,30 @@ function generatePDF() {
         return
     }
 
-    //Ação da Jaguar
+    //Descrever problema
+
+    if(document.getElementById("ProblemaCampoDeTexto").value != ""){
+        text += document.getElementById("ProblemaCampoDeTexto").value + "\n"
+    }else{
+        alert("Preencha o campo de problema!")
+        return
+    }
+
+    //Ação tomada
 
     if(document.getElementById("TrocarEmbalagem").checked){
-        text += document.getElementById("TrocarEmbalagem").value
+        text += document.getElementById("TrocarEmbalagem").value + "\n"
     }else if(document.getElementById("RepararEmbalagem")){
-        text += document.getElementById("RepararEmbalagem").value
+        text += document.getElementById("RepararEmbalagem").value + "\n"
     }else if(document.getElementById("SeguirDanificada").checked){
-        text += document.getElementById("SeguirDanificada").value
+        text += document.getElementById("SeguirDanificada").value + "\n"
     }
     else{
         alert("Marque a ação da Jaguar!")
     }
+
+    //Anexar imagem
+
     EffectButton()
     
     const jsPDF = window.jspdf.jsPDF;    
