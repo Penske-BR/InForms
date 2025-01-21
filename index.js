ca53bffec6cb880ac909383c2481ea88d8b4a822
Gabriel Matias <ggezcom991gh@gmail.com>
2025-01-21 18:51:55 -0300

    Reformulando o design do PDF e adicionando novos campos para o formulário.

--- index.js
+++ index.js
@@ -145,14 +145,28 @@ function RemoverImagens() {
 }
 
 function GerarPDF() {
-    RelatorioObject()
     EffectButton()
+
+            RelatorioObj = {
+        DataAtual: "11/02/2007",
+        NF: "178290",
+        Estado: "Bahia",
+        Cidade: "Xique-Xique",
+        Concessionaria: "BOX-Concessionária",
+        Item: "Parachoque",
+        Embalagem: "Papelão",
+        Problema: "Peça quebrada",
+        AcaoTomada: "Seguiu danificada",
+        PecaAvariada: "Sim",
+        Transportadora: "Jolivann",
+        Placa: "OSKM2939"
+    }
 
     const jsPDF = window.jspdf.jsPDF;    
     var doc = new jsPDF({
-        orientation: 'landscape',
+        orientation: 'portrait',
         unit: "mm",
-        format: [1008, 859] //x, y
+        format: [958, 1500] //x, y
     });
 
     // var ImagemAnexada = document.getElementsByClassName("ImagensAnexadas")
@@ -168,122 +182,150 @@ function GerarPDF() {
     doc.setFont("Helvetica","bold")
     doc.setLineWidth(2)
 
-    //Linha 1 horizontal (Margem)
-    doc.line(5, 5, 1003, 5)
+    //Margem
 
-    //Linha 1 vertical (Margem)
-    doc.line(5, 4, 5, 754)
+        //Superior
+        doc.line(50, 100, 908, 100)
 
-    //Linha 2 horizontal (Margem)
-    doc.line(5, 754, 1003, 754)
+        //Esquerda
+        doc.line(50, 100, 50, 1100)
 
-    //Linha 2 vertical (Margem)
-    doc.line(1003, 5, 1003, 754)
+        //Direita
+        doc.line(908, 100, 908, 1100)
 
-    //Linha abaixo da logo Penske
-    doc.addImage(LogoPenske, 10,0)
-    doc.line(5, 100, 1003, 100)
+        //Inferior
+        doc.line(50, 1100, 908, 1100)
 
-    //Linha ao lado da logo Penske
-    doc.line(130, 5, 130, 100)
 
-    //Linha entre tipo de documento e titulo
-    doc.line(130, 50, 873, 50)
-    doc.text("Tipo de documento:", 135, 35)
-    doc.text("Titulo:", 135, 75)
+    //Cabeçalho
 
-    //Linha ao lado da Revisão
-    doc.line(873, 5, 873, 100)
-    doc.text("Revisão:", 905, 60)
+        doc.addImage(LogoPenske,"PNG", 75, 78, 120, 120)
 
-    //Linha azul abaixo da logo Penske
-    doc.setDrawColor(33, 81, 168)
-    doc.setLineWidth(15)
-    doc.line(5, 105, 1003, 105)
+        //Linha azul abaixo do Logo Penske
+        doc.setDrawColor(32, 79, 146)
+        doc.setLineWidth(20)
+        doc.line(50, 180, 908, 180)
 
-    doc.setDrawColor(0, 0, 0)
-    doc.setLineWidth(2)
+        //Divisória entre logo e  cabeçalho
+        doc.setDrawColor(0,0,0)
+        doc.setLineWidth(1)
+        doc.line(220, 100, 220, 170)
 
-    //data e N° Controle
-    doc.text("Data:  " + RelatorioObj.DataAtual, 10, 130)
-    doc.text("N° Controle: ", 800, 130)
-    doc.line(876, 131, 998, 131)
+        //Tipo de Documento e Título
+        doc.text("Tipo de Documento: ", 225, 113)
+        doc.text("FOR.NISS.OPE.01", 470, 133)
+        doc.line(220, 135, 788, 135)
+        doc.text("Título: ", 225, 148)
+        doc.text("Relatório de Inspeção", 460, 168)
 
-    //Ação tomada
-    doc.text("Embalagem avariada: " + RelatorioObj.Embalagem, 10, 180)
-    doc.text("Ação tomada: " + RelatorioObj.AcaoTomada , 350, 180)
+        //Revisão
+        doc.line(788, 100, 788, 170)
+        doc.text("Revisão: 00", 810, 140)
 
-    //Peça avariada e problema
-    doc.text("Peça/Embalagem avariada: " + RelatorioObj.PecaAvariada, 10, 250)
-    doc.text("Descrever problema: " + RelatorioObj.Problema, 350, 250)
 
-    //Tansportadora, placa e destino
-    doc.text("Transportadora: ", 10, 350)
-    doc.text(RelatorioObj.Transportadora, 130, 350)
-    doc.text("Placa: " + RelatorioObj.Placa, 350, 350)
+    //Informações gerais
 
-    //Tabela abaixo do horário de saída
-    doc.setTextColor(255, 255, 255)
-    doc.line(5, 450, 1003, 450)
+        //Data e Agrupador
+        doc.text("Data:  01/02/2024", 65, 225)
+        doc.line(108, 229, 220, 229)
+        doc.text("N° da NF:  545123", 700, 225)
+        doc.line(770, 229, 908, 229)
 
-    doc.setLineWidth(20)
-    doc.setDrawColor(33, 81, 168)
-    doc.line(5, 460, 178, 460)
-    doc.text("Numero da NF", 36, 465)
+        //Embalagem, Peça avariada e Problema
+        doc.text("Embalagem avariada:  Papelão", 65, 280)
+        doc.line(216, 282, 300, 282)
+        doc.text("Peça avariada:  Sim", 400, 280)
+        doc.line(504, 280, 600, 280)
+        doc.text("Problema:  Caixa amassada", 680, 280)
+        doc.line(756, 280, 908, 280)
 
-    doc.setLineWidth(4)
-    doc.setDrawColor(0,0,0)
-    doc.line(178, 450, 178, 496)
+        //Ação tomada, Transportadora e Placa
+        doc.text("Ação tomada:  Seguiu danificada", 65, 335)
+        doc.line(164, 335, 300, 335)
+        doc.text("Transportadora:  PENSKE", 400, 335)
+        doc.line(515, 335, 600, 335)
+        doc.text("Placa:  OVK-8293", 680, 335)
+        doc.line(730, 335, 908, 335)
 
-    doc.setLineWidth(20)
-    doc.setDrawColor(33, 81, 168)
-    doc.line(178, 460, 380, 460)
-    doc.text("Descrição do item", 212, 465)
+        //Destino e Horario de saída
+        doc.text("Horário de saída:  17:00", 65, 400)
+        doc.line(188, 400, 240, 400)
+        doc.text("Destino  HUB SP/RJ", 400, 400)
+        doc.line(458, 400, 600, 400)
 
-    doc.setLineWidth(4)
-    doc.setDrawColor(0,0,0)
-    doc.line(380, 450, 380, 496)
 
-    doc.setLineWidth(20)
-    doc.setDrawColor(33, 81, 168)
-    doc.line(380, 460, 660, 460)
-    doc.text("Nome da concessionária", 430, 465)
+    //Tabela
 
-    doc.setLineWidth(4)
-    doc.setDrawColor(0,0,0)
-    doc.line(660, 450, 660, 496)
+        //Linhas
+        doc.setDrawColor(32, 79, 146)
+        doc.setLineWidth(24)
+        doc.line(50, 460, 908, 460) //Linha Azul
 
-    doc.setLineWidth(20)
-    doc.setDrawColor(33, 81, 168)
-    doc.line(660, 460, 820, 460)
-    doc.text("Estado", 710, 465)
+        doc.setDrawColor(0,0,0)
+        doc.setLineWidth(2)
 
-    doc.setLineWidth(4)
-    doc.setDrawColor(0,0,0)
-    doc.line(820, 450, 820, 496)
+        doc.line(50, 500, 908, 500)//Linha inferior
 
-    doc.setLineWidth(20)
-    doc.setDrawColor(33, 81, 168)
-    doc.line(820, 460, 1003, 460)
-    doc.text("Cidade", 880, 465)
+        //Divisórias
+        doc.line(165, 450, 165, 500)//D1
+        doc.line(370, 450, 370, 500)//D2
+        doc.line(625, 450, 625, 500)//D3
+        doc.line(750, 450, 750, 500)//D4
 
-    //Linha de baixo da tabela
-    doc.setLineWidth(2)
-    doc.setDrawColor(0, 0, 0)
-    doc.line(5, 470, 1003, 470)
+        //Informações
+        doc.setTextColor(255, 255,  255)
+        doc.text("Agrupador", 75, 465)
+        doc.setTextColor(0,0,0)
+        doc.text("546789", 75, 490)
 
-    //Linha que contém as informações
-    doc.setTextColor(0,0,0)
-    doc.line(5, 496, 1003, 496)
-    doc.text(RelatorioObj.NF, 55, 490)
-    
-    doc.text(RelatorioObj.Item, 212, 490)
+        doc.setTextColor(255, 255, 255)
+        doc.text("Descrição do item", 190, 465)
+        doc.setTextColor(0, 0, 0)
+        doc.text("Para-choque", 190, 490)
 
-    doc.text(RelatorioObj.Concessionaria, 430, 490)
+        doc.setTextColor(255, 255, 255)
+        doc.text("Nome da Concessionária", 400, 465)
+        doc.setTextColor(0, 0, 0)
+        doc.text("Katana Veículos LTDA", 400, 490)
 
-    doc.text(RelatorioObj.Estado, 710, 490)
+        doc.setTextColor(255, 255, 255)
+        doc.text("Estado", 665, 465)
+        doc.setTextColor(0, 0, 0)
+        doc.text("GO", 665, 490)
 
-    doc.text(RelatorioObj.Cidade, 880, 490)
+        doc.setTextColor(255, 255, 255)
+        doc.text("Cidade", 785, 465)
+        doc.setTextColor(0, 0, 0)
+        doc.text("Goiania", 785, 490)
+
+    //Campo das imagens
+
+        //Linha azul
+        doc.setDrawColor(32, 79, 146)
+        doc.setLineWidth(24)
+        doc.line(50, 570, 908, 570)
+        doc.setTextColor(255,255,255)
+        doc.text("Fotos do produto avariado", 400, 575)
+
+        //Imagens
+        doc.setLineWidth(2)
+        doc.setDrawColor(0,0,0)
+        doc.addImage("./Imgs/Imagem1e2.png", 60, 590, 260, 150)
+        doc.line(370, 580, 370, 755)
+        doc.addImage("./Imgs/Imagem1e2.png", 420, 590, 260, 150)
+        doc.line(730, 580, 730, 755)
+        doc.addImage("./Imgs/Imagem.png", 775, 590, 120, 150)
+        doc.line(50, 754, 908, 754)
+
+        //Campo de obs
+        doc.setTextColor(0,0,0)
+        doc.text("OBS:", 60, 800)
+        doc.line(100, 800, 854, 800)
+        doc.line(100, 850, 854, 850)
+        doc.line(100, 900, 854, 900)
+        doc.line(100, 950, 854, 950)
+        doc.line(100, 1000, 854, 1000)
+        doc.line(100, 1050, 854, 1050)
 
     doc.save("Relatorio.pdf")
-    }
+}
